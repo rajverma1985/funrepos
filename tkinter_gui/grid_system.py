d2ab19b11072b32ad7f84014b4eebbe1711e9134
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-08-31 14:08:36 +0530

    grid system

--- tkinter_gui/grid_system.py
+++ tkinter_gui/grid_system.py
@@ -4,21 +4,24 @@ from tkinter import *
 window = Tk()
 window.title("Quiz Game")
 window.minsize(width=600, height=400)
+window.config(padx=20, pady=20)
 
 # creating labels
 new_label = Label(font=("Arial", 24, "bold"), text="Test Label")
 new_label.grid(column=0, row=0)
-
+new_label.config(padx=20, pady=20)
 
 # creating buttons, event on button click
 button = Button(text="Click Here")
 button.grid(column=1, row=1)
+button.config(padx=20, pady=20)
 
 # Entry for an item, basically entry field
 take_input = Entry(bd=2, width=10)
 take_input.grid(column=3, row=3)
 
+
 button = Button(text="New button")
 button.grid(column=2, row=0)
-
+button.config(padx=20, pady=20)
 window.mainloop()
