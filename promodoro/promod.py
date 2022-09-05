68c4e15d2d7af3a803ecba620f02853d0af3451c
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-09-05 21:41:02 +0530

    added countdown stuff and more TBD

--- promodoro/promod.py
+++ promodoro/promod.py
@@ -27,39 +27,38 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-def start_timer():
-    time.time()
+def countdown():
+    window.....
+def start_timer(var):
+    canvas.itemconfig(timer, text=var)
 
 
 # create base window for the app
 window = Tk()
 window.title("pomodoro")
-window.config(padx=100, pady=100, bg="YELLOW")
-canvas = Canvas(width=200, height=300, bg="YELLOW", highlightthickness=0)
-t_image = PhotoImage(file="./tomato.png")
+window.config(padx=100, pady=100, bg=YELLOW)
+canvas = Canvas(width=200, height=300, bg=YELLOW, highlightthickness=0)
+t_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 100, image=t_image)
-canvas.create_text(100, 110, text="00:00", font=("Arial", 20, "bold"))
+timer = canvas.create_text(100, 110, text="00:00", font=("Arial", 20, "bold"))
 canvas.grid(column=1, row=1)
 
 # Timer, clock and  labels
-timer_label = Label(font=("Arial", 16, "bold"), text="TIMER", bg=YELLOW)
+timer_label = Label(font=("Arial", 20, "bold"), text="TIMER", bg=YELLOW, fg=GREEN)
 timer_label.grid(column=1, row=0)
-tick_label = Label(font=("Arial", 16, "bold"), text="✓", bg=YELLOW)
+tick_label = Label(font=("Arial", 20, "bold"), text="✓", bg=YELLOW, fg=GREEN)
 tick_label.grid(column=1, row=3)
 
 
 # Start and reset buttons
-
-
 def reset_timer():
     pass
 
 
 # creating buttons, event on button click
-start_button = Button(text="Start", command=start_timer)
-reset_button = Button(text="Reset", command=reset_timer)
-
+start_button = Button(height=2, width=4, text="Start", bd=0, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
+reset_button = Button(height=2, width=4, text="Reset", bd=0, highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 # Just in case Entry
