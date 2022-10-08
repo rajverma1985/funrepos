d3a3a500eecd3ddc8a8c1d85bf167b466923680f
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-10-08 23:31:43 +0530

    send email added to quotes

--- motivaltional_quotes/motivation.py
+++ motivaltional_quotes/motivation.py
@@ -1,10 +1,43 @@
 import datetime
+import os
+import random
 import smtplib
+import calendar
 
-now = datetime.datetime.now()
+from dotenv import load_dotenv
 
-quote_list = []
-with open('quotes.md', 'r') as quotes:
-    quote_list.append(quotes.readline())
+# load all env variables
+load_dotenv()
 
-print(quote_list[0])
+now = datetime.datetime.today()
+day_of_week = calendar.day_name[now.weekday()]
+
+
+def quote():
+    with open('quotes.md', 'r') as quotes:
+        quote_list = quotes.readlines()
+        return random.choice(quote_list)
+
+
+message = f"""From: Raj Verma <noreply@rajverma.com>
+To: {os.getenv('my_email')}
+Subject: QUOTE of the day for {day_of_week}
+
+{quote()}
+Have  Wonderful Day ahead!
+"""
+
+
+def send_email():
+    with smtplib.SMTP_SSL("smtp.gmail.com", port=465) as connection:
+        # secure the connection(packets sent using ttls)
+        connection.ehlo()
+        connection.login(user=os.getenv('user'), password=os.getenv('password'))
+        connection.sendmail(
+            from_addr=os.getenv('my_email'),
+            to_addrs=os.getenv('my_email'),
+            msg=message)
+
+
+if now.weekday() == 5:
+    send_email()
