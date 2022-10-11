8dce524a95c031e7a04815b1cdcece7f17198699
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-10-12 00:21:45 +0530

    letter logic TBD

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -1,17 +1,30 @@
 from datetime import datetime
-##################### Extra Hard Starting Project ######################
+import pandas as pd
 
-# 1. Update the birthdays.csv
+# 1. Update the birthdays.csv -- Done!
 
 # 2. Check if today matches a birthday in the birthdays.csv
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual
+# name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
 
-with open('birthdays.csv') as day:
-    print(day.read())
+df = pd.read_csv('birthdays.csv')
+birth_dates = (datetime.now().day, datetime.now().month)
+
+birthday = {(data.day, data.month, ): data for (index, data) in df.iterrows()}
+
+
+if birth_dates in birthday:
+    person = birthday[birth_dates]
+    # get the name and replace that persons anme with a random letter
+
+
+
+
+
 
 
 
